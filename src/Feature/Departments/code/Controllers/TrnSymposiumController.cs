a825ff315f273f4dd0bd987b05ad6badaa66fa48
Akhchaya Priya D <akhchayapriyaa.dhanasekaran@verticurl.com>
2021-06-07 11:46:22 +0530

    Added ECE Symposium

---
+++ src/Feature/Departments/code/Controllers/TrnSymposiumController.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.Data.Fields;
+using Sitecore.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Trn.Feature.Departments.Models;
+
+namespace Trn.Feature.Departments.Controllers
+{
+    public class TrnSymposiumController : Controller
+    {
+        // GET: SampleTrn
+        public ActionResult Index()
+        {
+            var contextItem = Sitecore.Context.Item;
+
+            SymposiumInfo symposiumInfo = new SymposiumInfo();
+
+          
+            symposiumInfo.SPOCPhoneNumber = new HtmlString(FieldRenderer.Render(contextItem, "SPOCPhoneNumber"));
+            symposiumInfo.PaperSubmissionLastDate = new HtmlString(FieldRenderer.Render(contextItem, "PaperSubmissionLastDate"));
+            symposiumInfo.SymposiumDescription = new HtmlString(FieldRenderer.Render(contextItem, "SymposiumDescription"));
+
+            return View(symposiumInfo);
+        }
+    }
+}
