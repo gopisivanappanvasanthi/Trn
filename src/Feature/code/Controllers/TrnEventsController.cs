2679719e5636703e171677fec53b19056fc3c4ba
Kohila C <kohila.chellasamy@verticurl.com>
2021-06-09 23:43:38 +0530

    Annual and Sports Day Event Page

---
+++ src/Feature/code/Controllers/TrnEventsController.cs
@@ -0,0 +1,29 @@
+﻿using Trn.Feature.Events.Models;
+using Sitecore.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Trn.Feature.Events.Controllers
+{
+    public class TrnEventsController : Controller
+    {
+        // GET: Events
+        public ActionResult Index()
+        {
+            var contextItem = Sitecore.Context.Item;
+
+            EventsInfo eventsInfo = new EventsInfo();
+
+
+            eventsInfo.EventDate = new HtmlString(FieldRenderer.Render(contextItem, "EventDate"));
+            eventsInfo.EventDuration = new HtmlString(FieldRenderer.Render(contextItem, "EventDuration"));
+            eventsInfo.EventPOC = new HtmlString(FieldRenderer.Render(contextItem, "EventPOC"));
+            eventsInfo.EventAgenda = new HtmlString(FieldRenderer.Render(contextItem, "EventAgenda"));
+
+            return View(eventsInfo);
+        }
+    }
+}
---
+++ src/Feature/code/Models/EventsInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trn.Feature.Events.Models
+{
+    public class EventsInfo
+    {
+        public HtmlString CarouselItems { get; set; }
+        public HtmlString EventDate { get; set; }
+        public HtmlString EventDuration { get; set; }
+        public HtmlString EventPOC { get; set; }
+        public HtmlString EventAgenda { get; set; }
+    }
+}
