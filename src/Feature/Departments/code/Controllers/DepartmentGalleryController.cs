e2594345066afd7c8fba6bbdc211c1f60a0546b4
gopisv <gopi.sv@verticurl.com>
2021-06-09 19:31:27 +0530

    sym-card-ref

--- src/Feature/Departments/code/Controllers/DepartmentGalleryController.cs
+++ src/Feature/Departments/code/Controllers/DepartmentGalleryController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Sitecore.Data.Fields;
+using Sitecore.Links;
+using Sitecore.Web.UI.WebControls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Trn.Feature.Departments.Models;
 
 namespace Trn.Feature.Departments.Controllers
 {
@@ -11,7 +15,26 @@ namespace Trn.Feature.Departments.Controllers
         // GET: DepartmentGallery
         public ActionResult Index()
         {
-            return View();
+            var contextitem = Sitecore.Context.Item;
+
+            MultilistField symposiums = contextitem.Fields["symposiums"];
+
+            List<SymposiumCard> symposiumCards = symposiums
+                                                    .GetItems()
+                                                    .Select(x => new SymposiumCard
+                                                    {
+                                                        cardTitle = x.Name,
+                                                        cardImage = new HtmlString(getImageFromItem(x)),
+                                                        url = LinkManager.GetItemUrl(x),
+                                                    }).ToList();
+
+            return View(symposiumCards);
+        }
+
+        public string getImageFromItem(Sitecore.Data.Items.Item item)
+        {
+            var childImage = item.GetChildren().First();
+            return FieldRenderer.Render(childImage, "ImageGallery");
         }
     }
 }
---
+++ src/Feature/Departments/code/Models/SymposiumCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trn.Feature.Departments.Models
+{
+    public class SymposiumCard
+    {
+        public string cardTitle { get; set; }
+        public HtmlString cardImage { get; set; }
+        public string url { get; set; }
+    }
+}
