0f413a64960fe3d3324cce5e2fae5ecb4b5356da
Mohana Priya K S <mohanapriya.subramani@verticurl.com>
2021-06-10 15:32:47 +0530

    Created the Article

---
+++ src/Feature/Article/code/Controllers/TrnArticleController.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Trn.Feature.Article.Models;
+using Sitecore.Web.UI.WebControls;
+
+
+namespace Trn.Feature.Article.Controllers
+{
+    public class TrnArticleController : Controller
+    {
+        // GET: article
+        public ActionResult Index()
+        {
+            var contextItem = Sitecore.Context.Item;
+
+            ArticleInfo article = new ArticleInfo
+            {
+                Title = new HtmlString(FieldRenderer.Render(contextItem, "Title")),
+                Description = new HtmlString(FieldRenderer.Render(contextItem, "Description")),
+                Image = new HtmlString(FieldRenderer.Render(contextItem, "Image"))
+            };
+
+            return View(article);
+        }
+    }
+}
