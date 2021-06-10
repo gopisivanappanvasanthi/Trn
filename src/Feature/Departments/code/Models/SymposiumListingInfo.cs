6bf3f895219e3e683662c0bd16d5a0df68e01023
Akhchaya Priya D <akhchayapriyaa.dhanasekaran@verticurl.com>
2021-06-10 18:13:49 +0530

    Added Symposium Cards

---
+++ src/Feature/Departments/code/Models/SymposiumListingInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trn.Feature.Departments.Models
+{
+    public class SymposiumListingInfo
+    {
+        public List<SymposiumCard> SymposiumCards { get; set; }
+    }
+}
