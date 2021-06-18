044e30559bb9ea4b98d9eba35b36b25b71211f05
Akhchaya Priya D <akhchayapriyaa.dhanasekaran@verticurl.com>
2021-06-18 20:42:01 +0530

    Commited QandA changes

---
+++ src/Feature/Home/code/Controllers/QandAController.cs
@@ -0,0 +1,58 @@
+﻿using Sitecore.Data;
+using Sitecore.SecurityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Trn.Feature.Home.Models;
+
+
+
+
+namespace Trn.Feature.Home.Controllers
+{
+    public class QandAController : Controller
+    {
+        // GET: QandA
+        [HttpGet]
+        public ActionResult Index()
+        {
+            QandA query = new QandA
+            {
+                Question = "",
+            };
+           
+            return View(query);
+        }
+
+        [HttpPost]
+        public ActionResult Index(QandA inputQuery)
+        {
+            var parentItem = Sitecore.Context.Item;
+            var masterDatabase = Sitecore.Configuration.Factory.GetDatabase("master");
+            var web = Sitecore.Configuration.Factory.GetDatabase("web");
+
+            var parentItemFromMaster = masterDatabase.GetItem(parentItem.ID);
+            ID id = new ID("{8CFC3B0F-B415-4152-B097-F4F6F64D0080}");
+            TemplateID templateID = new TemplateID(id);
+            using (new SecurityDisabler())
+            {
+                var createdItem = parentItemFromMaster.Add(inputQuery.Question, templateID);
+                createdItem.Editing.BeginEdit();
+                createdItem.Fields["question"].Value = inputQuery.Question;
+                createdItem.Fields["answers"].Value = inputQuery.Answers;
+                
+                createdItem.Editing.EndEdit();
+                createdItem.Editing.AcceptChanges();
+
+                //TrnPublishing trnPublishing = new TrnPublishing();
+                //trnPublishing.DoTrnPublish(createdItem, masterDatabase, web);
+
+            }
+
+            return View("/Views/QandA/QandA_SummaryOne.cshtml");
+
+        }
+    }
+}
---
+++ src/Feature/Home/code/Models/QandA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trn.Feature.Home.Models
+{
+    public class QandA
+    {
+        public string Question { get; set; }
+        public string Answers { get; set; }
+    }
+}
