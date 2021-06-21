25a83cbaf389c43c3f048250107da0f9fa35dbea
Kohila C <kohila.chellasamy@verticurl.com>
2021-06-21 13:21:25 +0530

    Admission Form - Date Picker added

--- src/Feature/Home/code/Controllers/TrnAdmissionFormController.cs
+++ src/Feature/Home/code/Controllers/TrnAdmissionFormController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Trn.Feature.Home.Models;
-//using Trn.Foundation.Publishing.Services;
+using Trn.Foundation.Publishing.Services;
 
 namespace Trn.Feature.Home.Controllers
 {
@@ -38,9 +38,9 @@ namespace Trn.Feature.Home.Controllers
             //var parentItemFromWeb = webDatabase.GetItem(parentItem.ID);
 
             var parentItemFromMaster = masterDatabase.GetItem(parentItemID);
-            ID idparentitem = new ID("{9F176A86-AFC9-488C-96A4-D801831D4040}");
-            var parentItemForAdmission = masterDatabase.GetItem(idparentitem);
-            TemplateID templateID = new TemplateID(idparentitem);
+            ID admissionformtemplateid = new ID("{9F176A86-AFC9-488C-96A4-D801831D4040}");
+           // var parentItemForAdmission = masterDatabase.GetItem(idparentitem);
+            TemplateID templateID = new TemplateID(admissionformtemplateid);
             using (new SecurityDisabler())
             {
                 var createdItem = parentItemFromMaster.Add(inputComment.FirstName, templateID);
@@ -55,8 +55,8 @@ namespace Trn.Feature.Home.Controllers
                 createdItem.Editing.EndEdit();
                 createdItem.Editing.AcceptChanges();
 
-               // TrnPublishing trnPublishing = new TrnPublishing();
-               //trnPublishing.DoTrnPublish(createdItem, masterDatabase, web);
+               TrnPublishing trnPublishing = new TrnPublishing();
+               trnPublishing.DoTrnPublish(createdItem, masterDatabase, web);
 
             }
             //Database master = Sitecore.Configuration.Factory.GetDatabase("master");
