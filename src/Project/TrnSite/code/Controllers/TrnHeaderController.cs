85a1b57cce0799f950767477cb3af78a82fd7b89
gopisv <gopi.sv@verticurl.com>
2021-07-02 16:45:29 +0530

    static binding example

---
+++ src/Project/TrnSite/code/Controllers/TrnHeaderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Trn.Project.TrnSite.Controllers
+{
+    public class TrnHeaderController : Controller
+    {
+        // GET: TrnHeader
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
