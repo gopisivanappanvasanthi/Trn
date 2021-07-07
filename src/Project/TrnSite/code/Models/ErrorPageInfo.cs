693fea45e76ab36c7ccd51bfe382114eae3b7f17
Akhchaya Priya D <akhchayapriyaa.dhanasekaran@verticurl.com>
2021-07-07 10:07:02 +0530

    404PageNotFound

---
+++ src/Project/TrnSite/code/Models/ErrorPageInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trn.Project.TrnSite.Models
+{
+    public class ErrorPageInfo
+    {
+        public HtmlString Title { get; set; }
+        public HtmlString Description { get; set; }
+        public HtmlString Image { get; set; }
+
+    }
+}
