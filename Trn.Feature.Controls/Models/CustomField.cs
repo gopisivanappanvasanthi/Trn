abcac750a3c9f8134bedf17cde9bfe918d0cb1b6
Arunkumar-Verticurl <Arunkumar.marimuthu@verticurl.com>
2021-07-14 15:22:01 +0530

    custom field changes commit

---
+++ Trn.Feature.Controls/Models/CustomField.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trn.Feature.Controls.Models
+{
+    public class CustomField
+    {
+        public int customDropdown { get; set; }
+    }
+}
