76dd043044f0956adc7b0a323d9922fffea793e5
Arunkumar-Verticurl <Arunkumar.marimuthu@verticurl.com>
2021-08-02 15:10:22 +0530

    static binding changes commit

---
+++ src/Project/TrnSite/code/Controllers/StaticPageController.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Trn.Project.TrnSite.Models;
+
+namespace Trn.Project.TrnSite.Controllers
+{
+    public class StaticPageController : Controller
+    {
+        // GET: StaticPage
+        public ActionResult Index()
+        {
+            var contextItem = Sitecore.Context.Item;
+            StaticPageInfo staticPage = new StaticPageInfo();
+            staticPage.Title = new HtmlString(FieldRenderer.Render(contextItem, "Title"));
+            staticPage.Description = new HtmlString(FieldRenderer.Render(contextItem, "Description"));            
+            return View(staticPage);
+        }
+    }
+}
---
+++ src/Project/TrnSite/code/Models/StaticPageInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trn.Project.TrnSite.Models
+{
+    public class StaticPageInfo
+    {
+        public HtmlString Title { get; set; }
+        public HtmlString Description { get; set; }
+    }
+}
