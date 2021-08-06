652adaf6fcde48596ec888adffc8c08908a8e4a0
Arunkumar-Verticurl <Arunkumar.marimuthu@verticurl.com>
2021-08-06 12:37:57 +0530

    commit of dynamic placeholder

---
+++ src/Project/TrnSite/code/Controllers/DynamicPlaceholderController.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Trn.Project.TrnSite.Controllers
+{
+    public class DynamicPlaceholderController : Controller
+    {
+        // GET: DynamicPlaceholder
+        public ActionResult Index()
+        {
+            Database masterDB = Sitecore.Context.Database;
+            Item studentsItem = masterDB.GetItem("{75F0BD78-BC6B-4F48-8A54-06588F42F025}");
+            List<Item> students = studentsItem.GetChildren().ToList();
+            return View(students);
+        }
+    }
+}
